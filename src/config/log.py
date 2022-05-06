508e50f907d18852b3c8f77b280943c4c0c18de1
Tony Truong <serinth@gmail.com>
2022-05-07 08:04:03 +1000

    removed log propagation

--- src/config/log.py
+++ src/config/log.py
@@ -10,5 +10,8 @@ def get_logging_handler():
 def setup_logger(name: str, config: Config):
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG if config.debug == True else logging.INFO)
+    if logger.hasHandlers():
+        logger.handlers.clear()
+    logger.propagate = False
     logger.addHandler(get_logging_handler())    
     return logger
